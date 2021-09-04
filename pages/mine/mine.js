9aa1815d7d2db008b7fe469752393ba1d5fbd1fe
Jobove <OYCuBa@gmail.com>
2021-09-05 02:29:50 +0800

    4th September

---
+++ pages/mine/mine.js
@@ -0,0 +1,28 @@
+Page({
+  data: {
+    canIGetUserData: false,
+    hasUserInfo: false,
+    userInfo: {}
+  },
+
+  onLoad: function () {
+    if(wx.getUserProfile) {
+      this.setData(
+        {
+          canIGetUserData: true
+        })
+    }
+  },
+  
+  getUserProfile(e) {
+    wx.getUserProfile({
+      desc: '获取用户信息',
+      success: (res) => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    })
+  }
+})
